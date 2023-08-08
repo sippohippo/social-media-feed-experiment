698ecf66eda2d705752dd207edc34423512ea856
sippohippo <75213741+sippohippo@users.noreply.github.com>
2023-08-08 13:57:27 +0200

    fixed bug in experiment.html

--- experimenttools.py
+++ experimenttools.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import text
 
 def select_posts(n_posts):
     query = text("""
-            SELECT p.name, p.handle, p.content, p.type, i.id
+            SELECT p.name, p.handle, p.content, p.type, i.id, p.id
             FROM posts p, images i
             WHERE p.profilePicId = i.id
             """)
@@ -12,6 +12,7 @@ def select_posts(n_posts):
     posts = sample(result, n_posts)
     shuffle(posts)
     return posts
+
 
 def select_image(id):
     query = text("""
@@ -22,3 +23,19 @@ def select_image(id):
     result = db.session.execute(query, {"id":id})
     data = result.fetchone()[0]
     return data
+
+
+def record_votes(profiles, votes):
+
+    profilesIds = []
+    for i in profiles:
+        profilesIds.append(i[5])
+
+    print(profilesIds)
+
+    # query = text("""
+    #         INSERT INTO votes
+    #         VALUES ()
+    #         """)
+    # db.session.execute(query, {"email":email, "password":hashed_password})
+    # db.session.commit()
